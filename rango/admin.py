d79f2453ce4b10eb0177324def13b20eba8fead3
shubbuu <sksinha1289@gmail.com>
2018-01-12 22:12:47 +0530

    my first django project

---
+++ rango/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from rango.models import Category, Page
+
+# Register your models here.
+# admin.site.register(Category)
+admin.site.register(Page)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category, CategoryAdmin)
---
+++ rango/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from rango.models import Category, Page
+from rango.forms import CategoryForm
+
+# Create your views here.
+def index(request):
+    # context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    # return render(request, 'rango/index.html', context=context_dict)
+
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'cate': category_list}
+    return render(request, 'rango/index.html', context_dict)
+
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+    try:
+         category = Category.objects.get(slug=category_name_slug)
+         pages = Page.objects.filter(category=category)
+         context_dict['pages'] = pages
+         context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context_dict)
+
+
+def add_category(request):
+    form = CategoryForm()
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+
+        else:
+            print(form.errors)
+
+    return render(request, 'rango/add_cate.html', {'form': form})
